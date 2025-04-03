2c8601d56d1437eb2939990605069b80604dd5b3
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-04-03 17:09:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,68 +24,8 @@ st.sidebar.markdown("<h2 style='color: #A67C52;'>J.A.R.V.I.S 🤖</h2>", unsafe_
 
 home, configuracoes = st.sidebar.tabs(['Home', 'Configurações'])
 
-with home:
-    st.markdown(
-        """
-        ## Bem-vindo ao J.A.R.V.I.S! 🤖
-        Esta aplicação permite interações com modelos de IA da OpenAI, proporcionando respostas inteligentes e contextualizadas para suas perguntas.
-
-        🔹 **Como funciona?**
-        
-        ✅ Insira sua chave da API OpenAI na aba Configurações. 
-        ✅ Preencha o campo "Instrução" para direcionar o assistente. 
-        ✅ Escolha entre os modelos GPT-3.5-Turbo e GPT-4. 
-        ✅ Faça o upload de um arquivo CSV para análise. 
-        ✅ Digite sua pergunta no chat e obtenha insights baseados nos dados fornecidos. 
-        """
-    )
-    st.sidebar.divider()
-    st.sidebar.markdown("Desenvolvido por [Leandro Souza](https://br.linkedin.com/in/leandro-souza-313136190)")
-
-with configuracoes:
-    if 'api_key' not in st.session_state:
-        st.session_state.api_key = None
-
-    api_key = st.text_input("API Key", type="password")
-
-    if api_key:
-        st.session_state.api_key = api_key
-        st.success('Chave salva com sucesso')
-
-    client = openai.OpenAI(api_key=st.session_state.api_key) if st.session_state.api_key else None
-
-    instruction = st.text_input("Instrução:")
-    selecao_modelo = st.selectbox("Escolha o modelo:", ['gpt-4o', 'gpt-4o-mini', 'gpt-3.5-turbo', 'gpt-3.5-turbo-0125'])
-    upload_file = st.file_uploader("Escolha um arquivo CSV", type=["csv"])
-
-    if st.button("Iniciar Assistente") and upload_file is not None:
-        criar_assistant()
-        criar_thread()
-        st.success("Assistente e Thread criados! Agora você pode fazer perguntas.")
-
-df = None
-if upload_file is not None:
-    try:
-        df = pd.read_csv(upload_file, sep=";", decimal=",")
-        if "Date" in df.columns:
-            df["Date"] = pd.to_datetime(df["Date"], errors='coerce')
-        if "Unnamed: 0" in df.columns:
-            df = df.drop(columns=["Unnamed: 0"])
-        df = df.sort_values("Date")
-        st.write("### Dados do CSV")
-        st.dataframe(df, use_container_width=True, hide_index=True)
-    except Exception as e:
-        st.error(f"Erro ao processar arquivo: {e}")
-
-if "assistant_id" not in st.session_state:
-    st.session_state.assistant_id = None
-if "thread_id" not in st.session_state:
-    st.session_state.thread_id = None
-
 def criar_assistant():
-    if st.session_state.assistant_id is None:
-        with upload_file as file:
-            openai_file = client.files.create(file=file, purpose="assistants")
+    if st.session_state.assistant_id is None and client:
         assistant = client.beta.assistants.create(
             name="Analista de Dados",
             instructions=instruction,
@@ -96,7 +36,7 @@ def criar_assistant():
     return st.session_state.assistant_id
 
 def criar_thread():
-    if st.session_state.thread_id is None:
+    if st.session_state.thread_id is None and client:
         thread = client.beta.threads.create()
         st.session_state.thread_id = thread.id
     return st.session_state.thread_id
@@ -144,6 +84,59 @@ def verifica_resposta(run):
                     st.pyplot(fig)
     else:
         st.error(f"Erro: {run.status}")
+
+with home:
+    st.markdown(
+        """
+        ## Bem-vindo ao J.A.R.V.I.S! 🤖
+        Esta aplicação permite interações com modelos de IA da OpenAI, proporcionando respostas inteligentes e contextualizadas para suas perguntas.
+
+        🔹 **Como funciona?**
+        
+        ✅ Insira sua chave da API OpenAI na aba Configurações. 
+        ✅ Preencha o campo "Instrução" para direcionar o assistente. 
+        ✅ Escolha entre os modelos GPT-3.5-Turbo e GPT-4. 
+        ✅ Faça o upload de um arquivo CSV para análise. 
+        ✅ Digite sua pergunta no chat e obtenha insights baseados nos dados fornecidos. 
+        """
+    )
+    st.sidebar.divider()
+    st.sidebar.markdown("Desenvolvido por [Leandro Souza](https://br.linkedin.com/in/leandro-souza-313136190)")
+
+with configuracoes:
+    if 'api_key' not in st.session_state:
+        st.session_state.api_key = None
+
+    api_key = st.text_input("API Key", type="password")
+
+    if api_key:
+        st.session_state.api_key = api_key
+        st.success('Chave salva com sucesso')
+
+    client = openai.OpenAI(api_key=st.session_state.api_key) if st.session_state.api_key else None
+
+    instruction = st.text_input("Instrução:")
+    selecao_modelo = st.selectbox("Escolha o modelo:", ['gpt-4o', 'gpt-4o-mini', 'gpt-3.5-turbo', 'gpt-3.5-turbo-0125'])
+    upload_file = st.file_uploader("Escolha um arquivo CSV", type=["csv"])
+
+df = None
+if upload_file is not None:
+    try:
+        df = pd.read_csv(upload_file, sep=";", decimal=",")
+        if "Date" in df.columns:
+            df["Date"] = pd.to_datetime(df["Date"], errors='coerce')
+        if "Unnamed: 0" in df.columns:
+            df = df.drop(columns=["Unnamed: 0"])
+        df = df.sort_values("Date")
+        st.write("### Dados do CSV")
+        st.dataframe(df, use_container_width=True, hide_index=True)
+    except Exception as e:
+        st.error(f"Erro ao processar arquivo: {e}")
+
+if "assistant_id" not in st.session_state:
+    st.session_state.assistant_id = None
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None
 
 pergunta = st.text_input("Perguntar ao arquivo:")
 if st.button("Enviar Pergunta") and pergunta and st.session_state.assistant_id and st.session_state.thread_id:
